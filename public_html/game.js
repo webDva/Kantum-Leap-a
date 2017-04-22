765ac7c302b4e3847a48ccf4b3f3129ca51435ba
jermaine <jaylarcane@gmail.com>
2017-04-22 13:57:27 -0400

    got gravity for block only!

--- public_html/game.js
+++ public_html/game.js
@@ -14,7 +14,20 @@ Game.prototype = {
     },
 
     create: function () {
+        // Display the block in the corner
         this.block = this.add.sprite(0, 0, 'placeholder');
+        
+        // Enable Arcade physics
+        this.physics.enable(this.block, Phaser.Physics.ARCADE);
+        
+        // Enable gravity for the block only
+        this.block.body.gravity.set(0, 180);
+        
+        // Make block collide with world bounds
+        this.block.body.collideWorldBounds = true;
+        
+        // Enable bounce for testing
+        this.block.body.bounce.set(1);
     },
 
     update: function () {
