d48219d13925f3a4ef34786954ab59be45369ae2
jermaine <jaylarcane@gmail.com>
2017-04-24 03:37:22 -0400

    End state now starts Menu state on text click

--- public_html/end.js
+++ public_html/end.js
@@ -12,8 +12,15 @@ var End = function (game) {
 End.prototype = {
     create: function () {
         // Display score
-        this.message = 'Your small planet lasted ' + this.game.state.states['Game'].timeElapsed + ' seconds long';
+        this.message = 'Your small planet lasted ' + this.game.state.states['Game'].timeElapsed + ' seconds' +
+                '\n\nCLICK TO PLAY AGAIN';
         this.displayText = this.add.text(this.world.centerX, this.world.centerY, this.message, {fill: '#ffffff'});
         this.displayText.anchor.setTo(0.5, 0.5);
+
+        // Go back to the menu screen on click or tap
+        this.displayText.inputEnabled = true;
+        this.displayText.events.onInputDown.add(function () {
+            this.state.start('Menu');
+        }, this);
     }
 };
